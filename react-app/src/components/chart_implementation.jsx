316a5c344f1bce77e97a4c45e7005b42778794f5
0x0leon <leonselz@proton.me>
2022-12-30 20:01:39 +0100

    new chart

---
+++ react-app/src/components/chart_implementation.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class chart_implementation extends Component {
+  render() {
+    return (
+      <div>chart_implementation</div>
+    )
+  }
+}
